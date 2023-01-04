a4bdb86024e106bdc4784577f50556f50fa47dca
Alex-Gombos <74148529+Alex-Gombos@users.noreply.github.com>
2023-01-04 14:41:08 +0200

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    sessions: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/v1/sessions');
+    console.log(response);
+    const body = await response.json();
+    this.setState({sessions: body});
+
+  }
+
+  render() {
+    const {sessions} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Sessions</h2>
+              {sessions.map(session =>
+                  <div key={session[["session_id"]]}>
+                    {session[["session_name"]]} ({session[["session_length"]]})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
